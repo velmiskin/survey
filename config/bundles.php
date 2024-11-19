6b2a18349a69676b3abab8ae8a1d0e2a694fd3a2
Sergei Velmiskin <velmiskin@gmail.com>
2024-11-19 20:58:29 +0100

    Add Maker Bundle

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
